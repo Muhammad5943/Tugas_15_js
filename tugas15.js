3bc9b71d0124daea484e2bd21a8b1421d684dfd0
Muhammad Aji Putra <muhammad.aji.putra.mp98@gmail.com>
2020-05-08 21:34:21 +0700

    tugas15js
    mengumpulkan tugas 15 js

---
+++ tugas15.js
@@ -0,0 +1,16 @@
+function pytagores() {
+    var ab = 8;
+    console.log("Nilai AB : ", ab);
+
+    var bc = 6;
+    console.log("Nilai BC : ", bc);
+
+    ac2 = Math.pow(ab,2) + Math.pow(bc,2);
+    
+    ac = Math.sqrt(ac2,2)
+
+    console.log("panjang sisi AC pada segitiga siki-siku tersebut adalah :", ac + " cm");
+    
+}
+
+pytagores();
